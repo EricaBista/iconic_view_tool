524b8f3460443acb894667a7c4108d324dbd4998
Erica Bista <erica.bista@sydney.edu.au>
2019-10-03 15:19:31 +1000

    gem creation

---
+++ lib/iconic_view_tool.rb
@@ -0,0 +1,7 @@
+require "iconic_view_tool/version"
+require "iconic_view_tool/renderer"
+
+module IconicViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iconic_view_tool/version.rb
@@ -0,0 +1,3 @@
+module IconicViewTool
+  VERSION = "0.1.0"
+end
